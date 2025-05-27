1aaca30d854e3244a164fe64cbf00f30f48a3c1a
Carlos Gomez <gcarloseduardo@hotmail.com>
2025-05-27 16:02:56 -0300

    incorporo_modulo try_except

--- mentoria_26-05.py
+++ mentoria_26-05.py
@@ -1,10 +1,12 @@
 import random
 
-'''piedra papel o tijera
+'''programo un juego de piedra papel o tijera, en donde el usuario pordrá elegir una opción de las siguientes, por consola:
 1 = piedra
 2 = papel
-3 = tijera'''
+3 = tijera
+y la máquina adptará un valor aleatorio importado del modulo ramdom metodo randint'''
 
+# defino una función objeto elegido, válido para ambos jugadores (máquina y jugador)
 def objeto_elegido(opcion):
     if opcion ==1:
         opcion = 'Piedra'
@@ -16,25 +18,28 @@ def objeto_elegido(opcion):
         opcion = 'Tijera'
         return opcion
 
+#defino una función de comparación cuando un jugador eligió Piedra y el otro Tijera.
 def piedra_rompe_tijera(mano_maquina,mano_jugador):
     if objeto_elegido(mano_maquina)=='Tijera' and objeto_elegido(mano_jugador) == 'Piedra':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana jugador! {objeto_elegido(mano_jugador)} rompe a {objeto_elegido(mano_maquina)} \n')
     elif objeto_elegido(mano_jugador)=='Tijera' and objeto_elegido(mano_maquina) == 'Piedra':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana máquina! {objeto_elegido(mano_maquina)} rompe a {objeto_elegido(mano_jugador)} \n')
 
-
+#defino una función de comparación cuando un jugador eligió Papel y el otro Tijera.
 def papel_envuelve_piedra(mano_maquina, mano_jugador):
     if objeto_elegido(mano_maquina)=='Papel' and objeto_elegido(mano_jugador) == 'Piedra':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana máquina! {objeto_elegido(mano_maquina)} envuelve a {objeto_elegido(mano_jugador)} \n')
     elif objeto_elegido(mano_jugador)=='Tijera' and objeto_elegido(mano_maquina) == 'Piedra':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana jugador! {objeto_elegido(mano_jugador)} envuelve a {objeto_elegido(mano_maquina)} \n')
 
+#defino una función de comparación cuando un jugador eligió Tijera y el otro Papel.
 def tijera_corta_papel(mano_maquina, mano_jugador):
     if objeto_elegido(mano_maquina)=='Tijera' and objeto_elegido(mano_jugador) == 'Papel':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana máquina! {objeto_elegido(mano_maquina)} corta a {objeto_elegido(mano_jugador)} \n')
     elif objeto_elegido(mano_jugador)=='Tijera' and objeto_elegido(mano_maquina) == 'Piedra':
         return print(f'\n máquina eligio {objeto_elegido(mano_maquina)}, y usted eligió {objeto_elegido(mano_jugador)}, gana jugador! {objeto_elegido(mano_jugador)} corta a {objeto_elegido(mano_maquina)} \n')
 
+# defino la función jugar()
 def jugar():
     mano_jugador = 0
     while mano_jugador != 4:
@@ -43,18 +48,14 @@ def jugar():
                     2 - Papel
                     3 - Tijera 
                     4 - Dejar de jugar\n''') 
+#Para el caso de que el usuario ingrese un valor inválido desde consola, utilizo un módulo try except. 
         try: 
             mano_jugador = int(input('Ingrese su opción elegida:'))
         except ValueError:
             print('\n la opción elegida no es un número, por favor elija nuevamente\n')
             mano_jugador = 0
-            print('''Para jugar al piedra, papel, tijera deberá elegir su mano. \n Elegir su opción \n
-                    1 - Piedra
-                    2 - Papel
-                    3 - Tijera 
-                    4 - Dejar de jugar\n''')
-            mano_jugador = int(input('Ingrese su opción elegida:'))
-        finally:
+            
+        else:
             mano_maquina = random.randint(1,3)
                     
             if mano_jugador == mano_maquina:
@@ -82,8 +83,6 @@ def jugar():
             elif mano_jugador == 4:
                 print('\n hasta pronto\n')
                 break
-            else:
-                print('\n la opción elegida es incorrecta, por favor elija nuevamente\n')
     
 
 jugar()
